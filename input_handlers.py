6a2ef4a024f98c5eb88f6a63d49292768b651023
punkideas <punkideas@gmail.com>
2020-08-17 19:32:18 -0600

    more functionality, consumables

--- input_handlers.py
+++ input_handlers.py
@@ -1,6 +1,20 @@
 import tcod as libtcod
 
-def handle_keys(key):
+from game_states import GameStates
+
+def handle_keys(key, game_state):
+    if game_state == GameStates.PLAYERS_TURN:
+        return handle_player_turn_keys(key)
+    elif game_state == GameStates.PLAYER_DEAD:
+        return handle_player_dead_keys(key)
+    elif game_state == GameStates.TARGETING:
+        return handle_targeting_keys(key)
+    elif game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
+        return handle_inventory_keys(key)
+
+    return {}
+
+def handle_player_turn_keys(key):
     # Movement keys
     if key.vk == libtcod.KEY_UP or key.vk == libtcod.KEY_KP8:
         return {'move': (0, -1)}
@@ -21,6 +35,16 @@ def handle_keys(key):
     elif key.vk == libtcod.KEY_KP5:
         return {'wait': None}
     
+
+    key_char = chr(key.c)
+    # Character actions
+    if key_char == 'g':
+        return {'pickup' : True}
+    elif key_char == 'i':
+        return {'show_inventory': True}
+    elif key_char == 'd':
+        return {'drop_inventory': True}
+    
     if key.vk == libtcod.KEY_ENTER and (key.lalt or key.ralt):
         # Alt+Enter Toggle full screen
         return {'fullscreen':True}
@@ -29,4 +53,53 @@ def handle_keys(key):
         # Exit game
         return {'exit':True}
     
+    return {}
+
+def handle_targeting_keys(key):
+    if key.vk == libtcod.KEY_ESCAPE:
+        return {'exit':True}
+
+    return {}
+
+def handle_player_dead_keys(key):
+
+    key_char = chr(key.c)
+
+    if key_char == 'i':
+        return {'show_inventory': True}
+
+    if key.vk == libtcod.KEY_ENTER and (key.lalt or key.ralt):
+        # Alt+Enter Toggle full screen
+        return {'fullscreen':True}
+    
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit game
+        return {'exit':True}
+
+    return {}
+
+
+def handle_inventory_keys(key):
+    index = key.c - ord('a')
+    
+    if index >= 0:
+        return {'inventory_index':index}
+    
+    if key.vk == libtcod.KEY_ENTER and (key.lalt or key.ralt):
+        # Alt+Enter Toggle full screen
+        return {'fullscreen':True}
+    
+    elif key.vk == libtcod.KEY_ESCAPE:
+        return {'exit':True}
+    
+    return {}
+
+def handle_mouse(mouse):
+    (x, y) = (mouse.cx, mouse.cy)
+
+    if mouse.lbutton_pressed:
+        return {'left_click':(x, y)}
+    elif mouse.rbutton_pressed:
+        return {'right_click':(x, y)}
+
     return {}
